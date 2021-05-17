a66cbecc01303a74fe24afc8345bdb91e1fc4418
Arthur <artur.kentbayev@gmail.com>
2021-05-17 17:05:50 +0600

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# zher.e-batys.kz
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+docker-compose up --build
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ web/src/views/Signup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row justify-content-center mt-5 mx-0">
+    <div class="col-lg-5 col-md-8 col-sm-10 col-12">
+      <div class="jumbotron bg-white py-4">
+        <transition name="fade" mode="out-in">
+          <component :is="signUpAction" />
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import UserData from '@/components/SignUp/UserData.vue';
+import VerifyEDS from '@/components/SignUp/VerifyEDS.vue';
+
+export default {
+  name: 'SignUp',
+  components: { UserData, VerifyEDS },
+  computed: {
+    ...mapGetters(['message', 'signUpAction', 'formUserData']),
+  },
+};
+</script>
